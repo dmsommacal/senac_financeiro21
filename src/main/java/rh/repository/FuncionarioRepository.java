40e25078eb949d65e849ddc19ef734552e1b717f
MarcusVVA <marcusvinicius20.almeida@gmail.com>
2024-07-09 20:44:37 -0300

    Adicionado a pesquisa por id no funcionarioDTO

--- src/main/java/rh/repository/FuncionarioRepository.java
+++ src/main/java/rh/repository/FuncionarioRepository.java
@@ -1,11 +1,15 @@
 package rh.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rh.enterprise.CustomQuerydslPredicateExecutor;
-import rh.model.FolhaPagamento;
 import rh.model.Funcionario;
+
+import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>, CustomQuerydslPredicateExecutor<Funcionario> {
+    List<Funcionario> findByNome(String nome);
 }
